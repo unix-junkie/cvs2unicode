09690da0ad3ac88aee3e55667a6aeb3d54df6358
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2014-07-25 17:01:34 +0400

    Initial UI enhancements

--- src/main/java/com/github/unix_junkie/cvs2unicode/ui/MainFrameFactory.java
+++ src/main/java/com/github/unix_junkie/cvs2unicode/ui/MainFrameFactory.java
@@ -3,14 +3,23 @@
  */
 package com.github.unix_junkie.cvs2unicode.ui;
 
+import static java.awt.GridBagConstraints.BOTH;
+import static java.awt.GridBagConstraints.HORIZONTAL;
+import static java.awt.GridBagConstraints.RELATIVE;
+import static java.awt.GridBagConstraints.REMAINDER;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.ListModel;
 
 /**
@@ -26,11 +35,42 @@ public final class MainFrameFactory {
 	 * @wbp.parser.entryPoint
 	 */
 	public static JFrame newInstance(final ListModel<String> listModel) {
+		final JPanel contentPane = new JPanel(new GridBagLayout());
+
+
+		final JProgressBar progressBar = new JProgressBar();
+		final GridBagConstraints progressBarConstraints = new GridBagConstraints();
+		progressBarConstraints.gridwidth = REMAINDER;
+		progressBarConstraints.weightx = 1.0;
+		progressBarConstraints.fill = HORIZONTAL;
+		contentPane.add(progressBar, progressBarConstraints);
+
+
 		final JList<String> list = new JList<>();
 		list.setModel(listModel);
 
-		final JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportView(list);
+		final JScrollPane listScrollPane = new JScrollPane();
+		listScrollPane.setViewportView(list);
+		final GridBagConstraints listConstraints = new GridBagConstraints();
+		listConstraints.gridwidth = REMAINDER;
+		listConstraints.gridheight = RELATIVE;
+		listConstraints.weightx = 1.0;
+		listConstraints.weighty = 1.0;
+		listConstraints.fill = BOTH;
+		contentPane.add(listScrollPane, listConstraints);
+
+
+		final JTable table = new JTable();
+
+		final JScrollPane tableScrollPane = new JScrollPane();
+		tableScrollPane.setViewportView(table);
+		final GridBagConstraints tableConstraints = new GridBagConstraints();
+		tableConstraints.gridwidth = REMAINDER;
+		tableConstraints.gridheight = REMAINDER;
+		tableConstraints.weightx = 1.0;
+		tableConstraints.weighty = 1.0;
+		tableConstraints.fill = BOTH;
+		contentPane.add(tableScrollPane, tableConstraints);
 
 		final JFrame frmCvsunicode = new JFrame();
 		frmCvsunicode.setTitle("cvs2unicode");
@@ -38,7 +78,7 @@ public final class MainFrameFactory {
 		frmCvsunicode.setPreferredSize(new Dimension(640, 900));
 		frmCvsunicode.setResizable(true);
 		frmCvsunicode.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frmCvsunicode.getContentPane().add(scrollPane);
+		frmCvsunicode.getContentPane().add(contentPane);
 
 		return frmCvsunicode;
 	}
